982efc064d73b78f4ae91adb15792479ba56ee92
Anthony Atkinson <ep1cantw0n@gmail.com>
2023-10-22 16:10:53 -0500

    first commit

---
+++ src/main/java/com/comp413/clientapi/ClientapiApplication.java
@@ -0,0 +1,13 @@
+package com.comp413.clientapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/comp413/clientapi/ClientapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp413.clientapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
