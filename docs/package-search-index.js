10cabd7e20a4403d0752988b5c9565f0ca6e3561
Anthony Atkinson <ep1cantw0n@gmail.com>
2023-10-23 00:26:53 -0500

    docs

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.comp413.clientapi.api"},{"l":"com.comp413.clientapi.obj"},{"l":"com.comp413.clientapi.server"}];updateSearchResults();
